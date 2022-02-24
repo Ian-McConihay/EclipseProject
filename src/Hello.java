6350db74b86a80c798002f1d2ce31932c517ffde
Ian McConihay <mcconihay.ian92@gmail.com>
2022-02-23 23:30:03 -0600

    SRC and Hello File

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		for(int i = 5; i < 10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
